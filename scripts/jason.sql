179715c3a42f379cd4f23b8f6bacb7d0049ef280
jrthompson25 <jrthompson35@outlook.com>
2022-02-19 09:43:17 -0600

    Project Complete

--- scripts/jason.sql
+++ scripts/jason.sql
@@ -81,11 +81,15 @@ FROM data_analyst_jobs
 WHERE title LIKE '%Analyst%';*/
 
 --************************Stuck on the last two
---12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
-/*SELECT DISTINCT title 
+--12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common? Data
+/* SELECT COUNT (DISTINCT title) 
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analy%';*/
+WHERE upper(title) NOT LIKE '%ANALY%';
 
+SELECT DISTINCT title
+FROM data_analyst_jobs
+WHERE upper(title) NOT LIKE '%ANALY%';
+*/
 
 --**BONUS**
 /*You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
